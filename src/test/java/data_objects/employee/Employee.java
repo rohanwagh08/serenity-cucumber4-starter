070d7bb7157cf2de53122c04ce7df804b93e4693
Rohan Wagh <rohan.madhukar.wagh@gmail.com>
2020-04-23 12:07:12 +0530

    Added second login scenario and parameterised the tests

---
+++ src/test/java/data_objects/employee/Employee.java
@@ -0,0 +1,9 @@
+package data_objects.employee;
+
+public class Employee {
+    private FullName fullname;
+    private int employeeId;
+    private String username;
+    private String password;
+    private boolean status;
+}
